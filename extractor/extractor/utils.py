22290ed34773dcfa76d26380e3b2562a619fe07d
jplumail <jeanplumail76@gmail.com>
2025-01-23 21:34:38 +0100

    refactor

--- extractor/extractor/utils.py
+++ extractor/extractor/utils.py
@@ -1,4 +1,25 @@
 import re
+import googleapiclient.discovery
+
 
 
 year_pattern = re.compile(r"\b\d{4}\b")
+
+API_KEY = "AIzaSyC-8WuhND8YDjvzZLNf0Jw8MGTQ3E1qCXA"
+
+def get_videos_playlist(playlist_id: str):
+    # Get credentials and create an API client
+    youtube = googleapiclient.discovery.build("youtube", "v3", developerKey=API_KEY)
+
+    playlistItems = youtube.playlistItems()
+    request = playlistItems.list(
+        part="id,contentDetails,status,snippet", playlistId=playlist_id
+    )
+
+    items = []
+    while request is not None:
+        items_page = request.execute()
+        items.extend(items_page["items"])
+        request = playlistItems.list_next(request, items_page)
+
+    return items
--- extractor/scripts/download_all_videos.py
+++ extractor/scripts/download_all_videos.py
@@ -1,3 +1,4 @@
+from extractor.utils import get_videos_playlist
 from tqdm import tqdm
 
 import googleapiclient.discovery
@@ -7,21 +8,8 @@ import yt_dlp
 from extractor.download import download_video
 
 
-API_KEY = "AIzaSyC-8WuhND8YDjvzZLNf0Jw8MGTQ3E1qCXA"
 
-# Get credentials and create an API client
-youtube = googleapiclient.discovery.build("youtube", "v3", developerKey=API_KEY)
-
-playlistItems = youtube.playlistItems()
-request = playlistItems.list(
-    part="id,contentDetails,status,snippet", playlistId="PL6yqY0TQJgwcSGYD6a2P4YgpHIp-HCzZn"
-)
-
-items = []
-while request is not None:
-    items_page = request.execute()
-    items.extend(items_page["items"])
-    request = playlistItems.list_next(request, items_page)
+items = get_videos_playlist("PL6yqY0TQJgwcSGYD6a2P4YgpHIp-HCzZn")
 
 for item in tqdm(items):
     if item["status"]["privacyStatus"] == "public":
