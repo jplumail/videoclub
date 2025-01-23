559cdc65b51ea3e6a45e33fb37f6b1a50ab0a6f3
jplumail <jeanplumail76@gmail.com>
2025-01-23 19:43:42 +0100

    use batch prediction to annotate videos

--- extractor/extractor/annotate_llm.py
+++ extractor/extractor/annotate_llm.py
@@ -1,15 +1,18 @@
+from datetime import datetime
 import json
 from typing import Any, List
+from google.genai.models import _GenerateContentParameters_to_vertex, _common
 from extractor.download import upload_json_blob
 import googleapiclient.discovery
 from pydantic import BaseModel
 from google import genai
 from google.genai import types
+from google.cloud import storage
 
 import jsonref
 
 client = genai.Client(
-    vertexai=True, project="videoclub-447210", location="us-central1"
+    vertexai=True, project="videoclub-447210", location="europe-west9"
 )
 
 TEMP = 0
@@ -73,6 +76,7 @@ def get_title(youtube_id: str):
 class NamesResponse(BaseModel):
     names: list[str]
 
+
 async def get_names_from_title(title: str):
     response = await client.aio.models.generate_content(
         model="gemini-2.0-flash-exp",
@@ -97,7 +101,7 @@ async def get_names_from_title(title: str):
         return NamesResponse.model_validate_json(response.text)
 
 
-async def annotate(video_blob_name: str):
+def create_request(video_blob_name: str):
     annotation_instruction = f"""Tu es un agent ContentID. Tu travailles pour une entreprise dans le domaine de la gestion des droits d'auteur.
 Ton rôle est de recenser les films qui passent dans les vidéos de la série Video Club de Konbini pour remplir une base de donnée.
 A chaque début d'annotation, il t'est donné une vidéo.
@@ -122,39 +126,118 @@ Ta réponse sera au format JSON suivant:
         temperature=TEMP,
         top_p=0.95,
         response_mime_type="application/json",
-        response_schema=to_vertexai_compatible_schema(AnnotationResponse.model_json_schema()),
+        response_schema=to_vertexai_compatible_schema(
+            AnnotationResponse.model_json_schema()
+        ),
         system_instruction=annotation_instruction,
         safety_settings=safety_settings,
     )
-    json_payload = ""
-    async for chunk in client.aio.models.generate_content_stream(
-        model="gemini-2.0-flash-exp",
-        contents=[types.Content(
-            role="user",
-            parts=[
-                types.Part.from_uri(video_blob_name, mime_type="video/*"),
-                types.Part.from_text("Annote"),
-            ],
-        )],
+    params = types._GenerateContentParameters(
+        contents=[
+            types.Content(
+                role="user",
+                parts=[
+                    types.Part.from_uri(video_blob_name, mime_type="video/*"),
+                    types.Part.from_text("Annote"),
+                ],
+            )
+        ],
         config=generation_config,
-    ):
-        print(chunk.text, end="")
-        if chunk.text:
-            json_payload += chunk.text
-    
-    return AnnotationResponse.model_validate_json(json_payload)
-
-
-async def annotate_video(bucket_name: str, video_blob_name: str, annotation_blob_name: str):
-    assert annotation_blob_name.endswith(".json")
-    result = await annotate(f"gs://{bucket_name}/{video_blob_name}")
-    blob_name = upload_json_blob(
-        bucket_name, result.model_dump_json(), annotation_blob_name
     )
-    return blob_name
 
+    vertex_params = _GenerateContentParameters_to_vertex(client._api_client, params)
+    request_dict = _common.convert_to_dict(vertex_params)
+    return {"request": request_dict}
+
+
+def create_batch_prediction_request_file(
+    bucket_name: str, video_blobs: list[str], request_blob_name: str
+) -> str:
+    assert request_blob_name.endswith(".jsonl")
+    requests = [
+        json.dumps(create_request(f"gs://{bucket_name}/{blob}"), ensure_ascii=False) for blob in video_blobs
+    ]
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(request_blob_name)
+    blob.upload_from_string("\n".join(requests), content_type="application/jsonl")
+    return blob.name  # type: ignore
+
+
+def create_batch_prediction_job(
+    bucket_name: str,
+    video_blobs: list[str],
+    request_blob_name: str,
+    destination_prefix: str,
+):
+    request_blob_name = create_batch_prediction_request_file(
+        bucket_name, video_blobs, request_blob_name
+    )
+    job = client.batches.create(
+        model="gemini-1.5-flash-002",
+        src=f"gs://{bucket_name}/{request_blob_name}",
+        config=types.CreateBatchJobConfig(
+            dest=f"gs://{bucket_name}/{destination_prefix}"
+        ),
+    )
+    return job.name
+
+
+import asyncio
+
+async def annotate_video(
+    bucket_name: str, video_blob_list: list[str], annotation_output_blob_list: list[str]
+):
+    job_id = int(datetime.now().timestamp())
+    job_prefix = f"work/{job_id}"
+    output_folder = f"{job_prefix}/output"
+    job_name = create_batch_prediction_job(
+        bucket_name,
+        video_blob_list,
+        f"{job_prefix}/annotations-request.jsonl",
+        output_folder,
+    )
+    if job_name is None:
+        raise Exception("Job creation failed")
+    job = client.batches.get(name=job_name)
+    completed_states = set(
+        [
+            "JOB_STATE_SUCCEEDED",
+            "JOB_STATE_FAILED",
+            "JOB_STATE_CANCELLED",
+            "JOB_STATE_PAUSED",
+        ]
+    )
+    while job.state is None or job.state not in completed_states:
+        await asyncio.sleep(10)
+        job = client.batches.get(name=job_name)
+        print(f"{datetime.now().isoformat()}: {job.state}")
+    
+    success_states = {"JOB_STATE_SUCCEEDED"}
+    if job.state not in success_states:
+        print(job)
+        raise Exception("Job failed")
+    
+    bucket = storage.Client().bucket(bucket_name)
+    blobs = bucket.list_blobs(prefix=output_folder)
+    prediction_blob = next(iter([
+        blob for blob in blobs 
+        if blob.name.endswith("predictions.jsonl")
+    ]))
+    print(f"Downloading {prediction_blob.name}")
+    with prediction_blob.open("r") as f:
+        for line, annotation_output_blob in zip(f, annotation_output_blob_list):
+            try:
+                response = json.loads(line)
+                json_payload = response["response"]["candidates"][0]["content"]["parts"][0]["text"]
+                AnnotationResponse.model_validate_json(json_payload)
+                upload_json_blob(bucket_name, json_payload, annotation_output_blob)
+            except Exception as e:
+                print(f"Error in response: {e}")
+                continue
 
 if __name__ == "__main__":
-    import sys
     import asyncio
-    asyncio.run(annotate_video(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    asyncio.run(annotate_video("videoclub-test", ["videos/xDNo7a48uOg/video.webm"], ["videos/xDNo7a48uOg/annotations.json"]))
