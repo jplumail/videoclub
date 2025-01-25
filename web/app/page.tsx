ca1910b64b900b829a44cd25b68278cd183c2418
jplumail <jeanplumail76@gmail.com>
2025-01-25 16:48:09 +0100

    ajout de la page d'accueil

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,95 +1,67 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use server";
 
-export default function Home() {
+import { Storage } from '@google-cloud/storage';
+
+function convertTitleToSlug(title: string): string {
+  return title
+    .toLowerCase()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/[^a-z0-9]+/g, '-')
+    .replace(/^-+|-+$/g, '');
+}
+
+interface VideoData {
+  snippet: {
+    publishedAt: string;
+    title: string;
+    videoId: string;
+    thumbnails: {
+      standard: {
+        url: string;
+        width: number;
+        height: number;
+      };
+    };
+    resourceId: {
+      videoId: string;
+    }
+  }
+}
+
+export default async function Home() {
+  const storage = new Storage();
+  const bucket = storage.bucket('videoclub-test');
+
+  const [files] = await bucket.getFiles({
+    prefix: 'videos/',
+  });
+
+  const jsonFiles = files.filter(file => file.name.endsWith('video.json'));
+  const videos: VideoData[] = [];
+
+  for (const file of jsonFiles) {
+    const [content] = await file.download();
+    const videoData = JSON.parse(content.toString()) as VideoData;
+    videos.push(videoData);
+  }
+
+  videos.sort((a, b) =>
+    new Date(b.snippet.publishedAt).getTime() - new Date(a.snippet.publishedAt).getTime()
+  );
+  console.log(videos[2]);
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <main>
+      {videos.map((video, index) => {
+        const videoUrl = `/video/${convertTitleToSlug(video.snippet.title)}_${video.snippet.resourceId.videoId}`;
+        return <div key={index}>
+          <a href={videoUrl}><img src={video.snippet.thumbnails.standard.url} alt="thumbnail" /></a>
+          <h2>
+            <a href={videoUrl}>{video.snippet.title}</a>
+          </h2>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      })}
+    </main>
   );
 }
