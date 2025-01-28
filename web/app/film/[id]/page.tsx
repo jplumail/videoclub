513ea5b157d7c7efee1b17fd0aeef821fb3f3918
jplumail <jeanplumail76@gmail.com>
2025-01-28 17:06:17 +0100

    refactor film page

--- web/app/film/[id]/page.tsx
+++ web/app/film/[id]/page.tsx
@@ -1,6 +1,5 @@
-import { PartialMedia, Person } from "@/lib/backend/types";
+import { Film } from "@/components/film";
 import { BucketManager } from "@/lib/data";
-import { slugify } from "@/lib/utils";
 
 export async function generateStaticParams() {
   const allMovies = await BucketManager.getMediaByPersonnalites();
@@ -11,43 +10,6 @@ export async function generateStaticParams() {
     }));
 }
 
-function Film({
-  movie,
-}: {
-  movie: {
-    movie: PartialMedia;
-    personnalites: {
-      person: Person;
-      videos: Set<string>;
-    }[];
-  };
-}) {
-  return (
-    <div>
-      <h1>{movie.movie.name}</h1>
-      <ul>
-        {movie.personnalites.map((personnalite, index) => (
-          <li key={index}>
-            <a href={`/personne/${personnalite.person.id}`}>
-              {personnalite.person.name}
-            </a>
-            {Array.from(personnalite.videos).map((videoId, key) => {
-              return (
-                <a
-                  key={key}
-                  href={`/video/${videoId}#${slugify(movie.movie.name || movie.movie.title || "")}`}
-                >
-                  <p>{videoId}</p>
-                </a>
-              );
-            })}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-
 export default async function Page({
   params,
 }: {
@@ -55,6 +17,10 @@ export default async function Page({
 }) {
   const movieId = (await params).id;
   const medias = await BucketManager.getMediaByPersonnalites();
-  const movie = medias.find((media) => media.movie.id?.toString() == movieId);
+  const movie = medias.find(
+    (media) =>
+      media.movie.media_type == "movie" &&
+      media.movie.id?.toString() == movieId,
+  );
   return movie && <Film movie={movie}></Film>;
 }
