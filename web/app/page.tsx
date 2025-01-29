7ea81efe9f764da6dae971e00fd530413643ed0f
jplumail <jeanplumail76@gmail.com>
2025-01-29 19:11:18 +0100

    amélioration style page d'accueil

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,24 +1,25 @@
 import { BucketManager } from "@/lib/data";
 import Image from "next/image";
+import styles from "./page.module.css";
 
 export default async function Home() {
   const videos = await BucketManager.getVideosSorted();
 
   return (
-    <main>
+    <main className={styles.videoContainer}>
       {videos.map((video, index) => {
         const videoUrl = `/video/${video.playlist_item.snippet.resourceId.videoId}`;
         const personnes = video.personnalites.filter((p) => p !== null);
         return (
-          <div key={index}>
-            <a href={videoUrl}>
+          <a key={index} className={styles.video} href={videoUrl}>
+            <div className={styles.thumbnail}>
               <Image
                 src={video.playlist_item.snippet.thumbnails.standard.url}
                 width={video.playlist_item.snippet.thumbnails.standard.width}
                 height={video.playlist_item.snippet.thumbnails.standard.height}
                 alt="thumbnail"
               />
-            </a>
+            </div>
             <h2>
               <a href={videoUrl}>{video.playlist_item.snippet.title}</a>
             </h2>
@@ -30,7 +31,7 @@ export default async function Home() {
                   </a>
                 );
               })}
-          </div>
+          </a>
         );
       })}
     </main>
