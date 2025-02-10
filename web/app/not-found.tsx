a4fbd41ca16b7c3ffa7177d8f660de491339887b
jplumail <jeanplumail76@gmail.com>
2025-02-10 17:11:21 +0100

    feat: ajouter une page Not Found personnalisée

---
+++ web/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div style={{margin: 'auto', textAlign: 'center'}}>
+      <h2>Page Introuvable</h2>
+      <p>La ressource demandée n&apos;a pas pu être trouvée</p>
+    </div>
+  )
+}
