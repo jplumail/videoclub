b5a6656082a13f014b31d5a593a16ac5d2c0860b
jplumail <jeanplumail76@gmail.com>
2025-02-10 16:40:10 +0100

    feat: améliorer le pied de page

--- web/components/footer.tsx
+++ web/components/footer.tsx
@@ -1,21 +1,39 @@
-import Image from "next/image";
 import styles from "./footer.module.css";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <span>
-        Les posters des films proviennent de{" "}
-        <Link
-          href="https://www.themoviedb.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          TMDB
-        </Link>
-        .
-      </span>
+      <div className={styles.footerContent}>
+        <span>
+          Les posters des films proviennent de{" "}
+          <Link
+            href="https://www.themoviedb.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            TMDB
+          </Link>
+          .
+        </span>
+        <span className={styles.copyright}>© 2025 Jean Plumail </span>
+        <span>
+          <Link
+            href="https://github.com/jplumail/videoclub/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/github-mark.svg"
+              alt="GitHub"
+              width={98}
+              height={96}
+              className={styles.githubIcon}
+            />
+          </Link>
+        </span>
+      </div>
     </footer>
   );
 }
